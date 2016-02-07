8366f79cd1c88a14c5d75ca316bc50ea35800ca1
Mason L <mason.lawlor@me.com>
2016-02-06 23:58:54 -0700

    blog page is looking good enough for now

--- site/web/app/themes/sage/templates/content-single.php
+++ site/web/app/themes/sage/templates/content-single.php
@@ -5,8 +5,9 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <h2 class="entry-title"><?php the_title(); ?></h2>
       <?php get_template_part('templates/entry-meta'); ?>
+      <?php the_post_thumbnail( 'featured-img-thumb' ); ?>
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
