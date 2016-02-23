fcac9838c5b4ea824ed5c04bff44ddbfd270aa9f
Mason L <mason.lawlor@me.com>
2016-02-22 20:13:46 -0700

    customizing home page– added titles

--- site/web/app/themes/sage/templates/homehero.php
+++ site/web/app/themes/sage/templates/homehero.php
@@ -3,14 +3,17 @@
     <!-- <svg class="a-b-screens" width="312" height="84"><?php get_template_part( 'assets/svg/inline', 'ab-screens.svg' ); ?></svg> -->
   </div>
   <div class="h-d-1 text-center">
-    <h1 class="h-h1-1 img-responsive">A Company made of</h1>
-    <h1 class="h-h1-2 img-responsive"><strong>people</strong></h1>
+    <h1 class="h-h1-1">An agency made of</h1>
+    <h1 class="h-h1-2"><strong>people</strong></h1>
     <div class="text-center">
       <h5 class="h-h5-1">and process</h5>
     </div>
   </div>
-  <div class="clients text-center">
-    <p>We've worked with:</p>
-    <svg width="1062" height="88" class="img-responsive"><?php get_template_part( 'assets/svg/inline', 'smclients.svg' ); ?></svg>
-  </div>
 </div>
+  <div class="clients text-center">
+    <small>
+      Web, video, & marketing.
+    </small>
+    <p>We've worked with:</p>
+    <svg width="1062" height="88"><?php get_template_part( 'assets/svg/inline', 'clients.svg' ); ?></svg>
+  </div>
--- site/web/app/themes/sage/templates/homesection1.php
+++ site/web/app/themes/sage/templates/homesection1.php
@@ -2,32 +2,106 @@
   <!-- Contact -->
   <!--         -->
   <div id="team">
-    <h5 class="text-center">our team</h5>
+    <h5 class="text-center">our team:</h5>
     <div class="row">
       <div class="col-xs-2">
         <center><a href="https://jackalopemedia.com/team/mason/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/mason-lawlor.jpg">
-        <p>Mason Lawlor</p></a></center>
+          <ul class="list-unstyled">
+            <li><p class="name">Mason</p></li>
+            <li><p class="title">Design</p></li>
+          </ul>
+      </a></center>
       </div>
       <div class="col-xs-2">
         <center><a href="https://jackalopemedia.com/team/michael/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/michael-collins.jpg">
-        <p>Michael Collins</p></a></center>
+          <ul class="list-unstyled">
+            <li><p class="name">Mike</p></li>
+            <li><p class="title">Copywriting</p></li>
+          </ul>
+        </a></center>
       </div>
       <div class="col-xs-2">
         <center><a href="https://jackalopemedia.com/team/jaron/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/jaronimo.jpg">
-        <p>Jaron Call</p></a></center>
+          <ul class="list-unstyled">
+            <li><p class="name">Mike</p></li>
+            <li><p class="title">Design</p></li>
+          </ul>
+        </a></center>
       </div>
       <div class="col-xs-2">
         <center><a href="https://jackalopemedia.com/team/timothy/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/timothy.jpg">
-        <p>Tim Dreckman</p></a></center>
+          <ul class="list-unstyled">
+            <li><p class="name">Tim</p></li>
+            <li><p class="title">Development</p></li>
+          </ul>
+        </a></center>
       </div>
       <div class="col-xs-2">
         <center><a href="https://jackalopemedia.com/team/skyler/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/skyler-foulger.jpg">
-        <p>Skyler Foulger</p></a></center>
+          <ul class="list-unstyled">
+            <li><p class="name">Skyler</p></li>
+            <li><p class="title">Design</p></li>
+          </ul>
+        </a></center>
       </div>
       <div class="col-xs-2">
         <center><a href="https://jackalopemedia.com/team/jeffrey/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/jeff-gil.jpg">
-        <p>Jeff Gilivary</p></a></center>
+          <ul class="list-unstyled">
+            <li><p class="name">Jeff</p></li>
+            <li><p class="title">Sound</p></li>
+          </ul>
+        </a></center>
       </div>
+    </div>
+    <!-- <div class="row">
+      <div class="col-xs-2">
+        <center><a href="https://jackalopemedia.com/team/mason/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/mason-lawlor.jpg">
+          <ul class="list-unstyled">
+            <li><p class="name">Mason</p></li>
+            <li><p class="title">Design</p></li>
+          </ul>
+      </a></center>
+      </div>
+      <div class="col-xs-2">
+        <center><a href="https://jackalopemedia.com/team/michael/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/michael-collins.jpg">
+          <ul class="list-unstyled">
+            <li><p class="name">Mike</p></li>
+            <li><p class="title">Copywriting</p></li>
+          </ul>
+        </a></center>
+      </div>
+      <div class="col-xs-2">
+        <center><a href="https://jackalopemedia.com/team/jaron/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/jaronimo.jpg">
+          <ul class="list-unstyled">
+            <li><p class="name">Mike</p></li>
+            <li><p class="title">Design</p></li>
+          </ul>
+        </a></center>
+      </div>
+      <div class="col-xs-2">
+        <center><a href="https://jackalopemedia.com/team/timothy/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/timothy.jpg">
+          <ul class="list-unstyled">
+            <li><p class="name">Tim</p></li>
+            <li><p class="title">Development</p></li>
+          </ul>
+        </a></center>
+      </div>
+      <div class="col-xs-2">
+        <center><a href="https://jackalopemedia.com/team/skyler/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/skyler-foulger.jpg">
+          <ul class="list-unstyled">
+            <li><p class="name">Skyler</p></li>
+            <li><p class="title">Design</p></li>
+          </ul>
+        </a></center>
+      </div>
+      <div class="col-xs-2">
+        <center><a href="https://jackalopemedia.com/team/jeffrey/"><img class="m-b img-circle img-responsive team-images" style="" src="http://jackalopemedia.com/app/uploads/2016/02/jeff-gil.jpg">
+          <ul class="list-unstyled">
+            <li><p class="name">Jeff</p></li>
+            <li><p class="title">Sound</p></li>
+          </ul>
+        </a></center>
+      </div> -->
     </div>
   </div>
 
