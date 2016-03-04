151c4e3eddd3b3ada4e755ad0ea9831a854b5508
Mason L <mason.lawlor@me.com>
2016-03-03 20:41:30 -0700

    home page ready for production-mvp

--- site/web/app/themes/sage/templates/homesection1.php
+++ site/web/app/themes/sage/templates/homesection1.php
@@ -5,51 +5,51 @@
     <h5 class="text-center">The team:</h5>
     <div class="row">
       <div class="col-xs-2">
-        <center class="team-img-bg"><a href="https://jackalopemedia.com/team/mason/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/mason-lawlor.jpg">
-          <!-- <ul class="list-unstyled">
+        <center><a href="https://jackalopemedia.com/team/mason/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/mason-lawlor.jpg">
+          <ul class="list-unstyled">
             <li><p class="name">Mason</p></li>
-            <li><p class="title">Design</p></li>
-          </ul> -->
+            <li><p class="title">Founder</p></li>
+          </ul>
       </a></center>
       </div>
       <div class="col-xs-2">
-        <center class="team-img-bg"><a href="https://jackalopemedia.com/team/jeffrey/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/neil-larson.jpg">
-          <!-- <ul class="list-unstyled">
+        <center><a href="https://jackalopemedia.com/team/jeffrey/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/neil-larson.jpg">
+          <ul class="list-unstyled">
             <li><p class="name">Neil</p></li>
-            <li><p class="title">Video</p></li>
-          </ul> -->
+            <li><p class="title">Director</p></li>
+          </ul>
         </a></center>
       </div>
       <div class="col-xs-2">
-        <center class="team-img-bg"><a href="https://jackalopemedia.com/team/michael/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/michael-collins.jpg">
-          <!-- <ul class="list-unstyled">
+        <center><a href="https://jackalopemedia.com/team/michael/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/michael-collins.jpg">
+          <ul class="list-unstyled">
             <li><p class="name">Mike</p></li>
             <li><p class="title">Copywriting</p></li>
-          </ul> -->
+          </ul>
         </a></center>
       </div>
       <div class="col-xs-2">
-        <center class="team-img-bg"><a href="https://jackalopemedia.com/team/jaron/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/jaronimo.jpg">
-          <!-- <ul class="list-unstyled">
+        <center><a href="https://jackalopemedia.com/team/jaron/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/jaronimo.jpg">
+          <ul class="list-unstyled">
             <li><p class="name">Jaron</p></li>
             <li><p class="title">Design</p></li>
-          </ul> -->
+          </ul>
         </a></center>
       </div>
       <div class="col-xs-2">
-        <center class="team-img-bg"><a href="https://jackalopemedia.com/team/timothy/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/timothy.jpg">
-          <!-- <ul class="list-unstyled">
+        <center><a href="https://jackalopemedia.com/team/timothy/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/timothy.jpg">
+          <ul class="list-unstyled">
             <li><p class="name">Tim</p></li>
             <li><p class="title">Development</p></li>
-          </ul> -->
+          </ul>
         </a></center>
       </div>
       <div class="col-xs-2">
-        <center class="team-img-bg"><a href="https://jackalopemedia.com/team/skyler/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/skyler-foulger.jpg">
-          <!-- <ul class="list-unstyled">
+        <center><a href="https://jackalopemedia.com/team/skyler/"><img class="m-b img-circle img-responsive team-images" style="" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/team/opti/skyler-foulger.jpg">
+          <ul class="list-unstyled">
             <li><p class="name">Skyler</p></li>
-            <li><p class="title">Design</p></li>
-          </ul> -->
+            <li><p class="title">Graphics</p></li>
+          </ul>
         </a></center>
       </div>
     </div>
